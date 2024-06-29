528f709e89c78ae725f7512f9c3a7f50faa83700
ambin04379 <praveen.g.vamsi@macys.com>
2024-06-29 18:25:42 +0530

    assignments

--- src/main/java/com/project/movie/services/MongoTemplateService.java
+++ src/main/java/com/project/movie/services/MongoTemplateService.java
@@ -3,12 +3,15 @@ package com.project.movie.services;
 import com.project.movie.entities.Ticket;
 import com.project.movie.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.MatchOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,8 +26,10 @@ public class MongoTemplateService {
     }
 
     public List<Ticket> findTicketPriceWithAggregation(String price) {
-        MatchOperation matchOperation = Aggregation.match(Criteria.where("totalTicketsPrice").gte(Integer.valueOf(price)));
-        Aggregation aggregation = Aggregation.newAggregation(matchOperation);
+        List<AggregationOperation> aggregationOperations = new ArrayList<>();
+        aggregationOperations.add(Aggregation.match(Criteria.where("totalTicketsPrice").gte(Integer.valueOf(price))));
+        aggregationOperations.add(Aggregation.sort(Sort.Direction.DESC, "totalTicketsPrice"));
+        Aggregation aggregation = Aggregation.newAggregation(aggregationOperations);
         return mongoTemplate.aggregate(aggregation, "tickets", Ticket.class).getMappedResults();
     }
 }
